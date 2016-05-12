5d0cf06af1cfebbc0f0d1371ad999b96b6578844
Graham Webber <gjrwebber@gmail.com>
2016-05-12 13:43:39 +1000

    Made a Schema Object a valid native query paramater.

--- spring-data-gremlin-orientdb/src/main/java/org/springframework/data/gremlin/query/orientdb/NativeOrientdbGremlinQuery.java
+++ spring-data-gremlin-orientdb/src/main/java/org/springframework/data/gremlin/query/orientdb/NativeOrientdbGremlinQuery.java
@@ -1,16 +1,20 @@
 package org.springframework.data.gremlin.query.orientdb;
 
+import com.orientechnologies.orient.core.command.OCommand;
+import com.orientechnologies.orient.core.command.OCommandRequest;
 import com.orientechnologies.orient.core.sql.OCommandSQL;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.gremlin.query.AbstractNativeGremlinQuery;
 import org.springframework.data.gremlin.query.GremlinQueryMethod;
 import org.springframework.data.gremlin.repository.GremlinGraphAdapter;
+import org.springframework.data.gremlin.schema.GremlinSchema;
 import org.springframework.data.gremlin.schema.GremlinSchemaFactory;
 import org.springframework.data.gremlin.tx.GremlinGraphFactory;
 import org.springframework.data.gremlin.tx.orientdb.OrientDBGremlinGraphFactory;
 import org.springframework.data.repository.query.DefaultParameters;
 import org.springframework.data.repository.query.Parameter;
 import org.springframework.data.repository.query.ParametersParameterAccessor;
+import org.springframework.util.Assert;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -34,38 +38,48 @@ public class NativeOrientdbGremlinQuery extends AbstractNativeGremlinQuery {
     @Override
     protected Object doRunQuery(DefaultParameters parameters, Object[] values, boolean ignorePaging) {
 
-        Map<String, Object> params = new HashMap<String, Object>();
         String queryString = query;
-        for (Object obj : parameters) {
-            Parameter param = (Parameter) obj;
-            Object val = values[param.getIndex()];
-            if (val == null || val instanceof Pageable) {
-                continue;
+        Map<String, Object> params = new HashMap<String, Object>();
+        if (parameters != null) {
+            for (Object obj : parameters) {
+                Parameter param = (Parameter) obj;
+                Object val = values[param.getIndex()];
+                if (val == null || val instanceof Pageable) {
+                    continue;
+                }
+                String paramName = param.getName();
+                String placeholder = param.getPlaceholder();
+                if (paramName == null) {
+                    paramName = "placeholder_" + param.getIndex();
+                    queryString = queryString.replaceFirst("\\?", paramName);
+                } else {
+                    queryString = queryString.replaceFirst(placeholder, paramName);
+                }
+
+                params.put(paramName, val);
             }
-            String paramName = param.getName();
-            String placeholder = param.getPlaceholder();
-            if (paramName == null) {
-                paramName = "placeholder_" + param.getIndex();
-                queryString = queryString.replaceFirst("\\?", paramName);
-            } else {
-                queryString = queryString.replaceFirst(placeholder, paramName);
+
+            // TODO: Hack until OLuceneSpatialIndexManager.searchIntersect handles the context
+            //        if (queryString.contains("$spatial")) {
+            for (String key : params.keySet()) {
+                queryString = queryString.replaceAll("\\b" + key + "\\b", convertObject(params.get(key)));
+            }
+            //        }
+
+            ParametersParameterAccessor accessor = new ParametersParameterAccessor(parameters, values);
+            Pageable pageable = accessor.getPageable();
+            if (pageable != null && !ignorePaging) {
+                queryString = String.format("%s SKIP %d LIMIT %d", queryString, pageable.getOffset(), pageable.getPageSize());
             }
 
-            params.put(paramName, val);
         }
 
-        // TODO: Hack until OLuceneSpatialIndexManager.searchIntersect handles the context
-        //        if (queryString.contains("$spatial")) {
-        for (String key : params.keySet()) {
-            queryString = queryString.replaceAll("\\b" + key + "\\b", convertObject(params.get(key)));
-        }
-        //        }
+        return run(queryString, params);
+    }
 
-        ParametersParameterAccessor accessor = new ParametersParameterAccessor(parameters, values);
-        Pageable pageable = accessor.getPageable();
-        if (pageable != null && !ignorePaging) {
-            queryString = String.format("%s SKIP %d LIMIT %d", queryString, pageable.getOffset(), pageable.getPageSize());
-        }
+    public Object run(String queryString, Map<String, Object> params) {
+
+        Assert.hasLength(queryString);
 
         try {
             Object result = orientGraphFactory.graph().command(new OCommandSQL(queryString)).execute(params);
@@ -89,7 +103,12 @@ public class NativeOrientdbGremlinQuery extends AbstractNativeGremlinQuery {
         } else if (val instanceof Float) {
             return val + "f";
         } else {
-            return val.toString();
+            GremlinSchema schema = schemaFactory.getSchema(val.getClass());
+            if(schema != null) {
+                return schema.getGraphId(val);
+            } else {
+                return val.toString();
+            }
         }
     }
 }
